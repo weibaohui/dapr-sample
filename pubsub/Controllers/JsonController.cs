3e7c8a44a90e1d9d3ecdb58982881bfcd42f2193
weibaohui <weibaohui@yeah.net>
2021-04-10 16:58:13 +0800

    param

--- pubsub/Controllers/JsonController.cs
+++ pubsub/Controllers/JsonController.cs
@@ -1,4 +1,3 @@
-using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Mvc;
 using pubsub.module;
 
@@ -6,26 +5,34 @@ namespace pubsub.Controllers
 {
     [ApiController]
     [Route("[Controller]")]
-    public class JsonController: ControllerBase
+    public class JsonController : ControllerBase
     {
         [HttpGet]
         [Route("/hello")]
         public CloudEvent HelloWorld()
         {
-            CloudEvent ce = new CloudEvent();
+            var ce = new CloudEvent();
             var ceData = new Data {Message = "MSG", MessageType = "TYPE"};
             ce.Data = ceData;
             ce.DataContentType = "json";
             ce.Id = "id";
             ce.PubSubName = "pubsubname";
-            
+
             return ce;
         }
+
         [HttpPost]
         [Route("/hello")]
         public CloudEvent DoHelloWorld(CloudEvent ce)
         {
             return ce;
+        }
+
+        [HttpPost]
+        [Route("/hello/{msg}/{age:int}")]
+        public string DoHelloWorld(string msg, int age)
+        {
+            return $"hello {msg} is {age} old";
         }
     }
 }
